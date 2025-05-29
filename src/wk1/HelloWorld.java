a768a124ad8ee67b821ce6cacf11e86f1f86c4a6
Dr. Taylor <taylor@msoe.edu>
2025-05-29 16:08:34 -0500

    Week 1 Lecture 3

--- src/wk1/HelloWorld.java
+++ src/wk1/HelloWorld.java
@@ -1,13 +1,12 @@
 package wk1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -15,12 +14,16 @@ public class HelloWorld extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label message = new Label("Hello World!");
-        message.setFont(new Font("Times New Roman", 50));
-        Pane box = new FlowPane();
-        box.getChildren().add(message);
-        box.getChildren().add(new Label("Hello universe"));
-        box.getChildren().add(new Button("Click Me!"));
+        Label message = new Label("label");
+        TextField tf = new TextField();
+        Button button = new Button("Click Me!");
+        tf.setOnAction(this::textFieldHandler);
+        button.setOnAction(this::buttonHandler);
+
+        GridPane box = new GridPane();
+        box.add(message, 0, 0);
+        box.add(tf, 1, 0);
+        box.add(button, 0, 1);
         Scene scene = new Scene(box);
 
         stage.setTitle("Title bar");
@@ -28,6 +31,14 @@ public class HelloWorld extends Application {
         stage.setHeight(400);
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void buttonHandler(ActionEvent actionEvent) {
+        System.err.println("button");
+    }
+
+    private void textFieldHandler(ActionEvent actionEvent) {
+        System.out.println("Textfield");
     }
 
     public static void main(String[] args) {
